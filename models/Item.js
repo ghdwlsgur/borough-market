b1d87746899b13c44450db22163323259ca42d3e
HongJinHyeok <vjinhyeokv@gmail.com>
2022-09-30 21:11:14 +0900

    객체에 담아서 전달

--- models/Item.js
+++ models/Item.js
@@ -12,4 +12,5 @@ const ItemSchema = new Schema({
     },
 });
 
-module.exports = Item = mongoose.model('item', ItemSchema);
+const Item = mongoose.model('item', ItemSchema);
+module.exports = { Item };
