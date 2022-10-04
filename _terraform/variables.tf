1f5b238a3c2b912a79ece262122f24266957c2a6
HongJinHyeok <vjinhyeokv@gmail.com>
2022-10-04 15:11:25 +0900

    use terraform.tfvars

--- _terraform/variables.tf
+++ _terraform/variables.tf
@@ -1,23 +1,10 @@
 
 
-variable "ECS_AMI" {
-  default = "ami-0b41652f00b442576"
-}
-
-variable "INSTANCE_TYPE" {
-  default = "t2.medium"
-}
-
-variable "AWS_REGION" {
-  default = "eu-central-1"
-}
-variable "AWS_RESOURCE_PREFIX" {
-  default = "borough-market"
-}
-
-variable "EMAIL" {
-  default = "redmax45@naver.com"
-}
+variable "ECS_AMI" {}
+variable "INSTANCE_TYPE" {}
+variable "AWS_REGION" {}
+variable "AWS_RESOURCE_PREFIX" {}
+variable "EMAIL" {}
 
 locals {
   aws_ecr_repository_name = var.AWS_RESOURCE_PREFIX
