bd37c4e3bf1ef47d3ed9f7b24333dc610ce0e980
HongJinHyeok <vjinhyeokv@gmail.com>
2022-10-08 15:21:01 +0900

    integrate tfvars

--- _terraform/variables.tf
+++ _terraform/variables.tf
@@ -1,10 +1,9 @@
 
-
-variable "ECS_AMI" {}
-variable "INSTANCE_TYPE" {}
-variable "AWS_REGION" {}
-variable "AWS_RESOURCE_PREFIX" {}
-variable "EMAIL" {}
+variable "ECS_AMI" { default = "ami-0b41652f00b442576" }
+variable "INSTANCE_TYPE" { default = "t2.medium" }
+variable "AWS_REGION" { default = "eu-central-1" }
+variable "AWS_RESOURCE_PREFIX" { default = "borough-market" }
+variable "EMAIL" { default = "redmax45@naver.com" }
 
 locals {
   aws_ecr_repository_name = var.AWS_RESOURCE_PREFIX
